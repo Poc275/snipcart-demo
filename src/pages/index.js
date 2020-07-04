1106582d8bb514673bef5ef8786e9108081cdc71
Peter O'Connor <poc275@gmail.com>
2020-07-04 13:22:19 +0100

    Removes commented out code.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-// import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import './index.scss'
@@ -9,57 +8,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <MainHomePage />
-    {/* {
-      data.allStrapiProduct.edges.map(product => (
-        <Product key={product.node.id} data={product.node} />
-      ))
-    } */}
   </Layout>
 )
 
 export default IndexPage
-
-// export const pageQuery = graphql`
-// {
-//   allStrapiProduct(filter: {shops: {elemMatch: {title: {eq: "demo_shop_one"}}}}) {
-//     edges {
-//       node {
-//         id
-//         weight
-//         variable
-//         title
-//         summary
-//         sku
-//         price
-//         description
-//         image {
-//           childImageSharp {
-//             fixed {
-//               ...GatsbyImageSharpFixed
-//             }
-//           }
-//         }
-//         variations {
-//           name
-//           options {
-//             description
-//             value
-//           }
-//         }
-//         category
-//         dimensions
-//         tags
-//         product_gallery {
-//           image {
-//             childImageSharp {
-//               fixed {
-//                 ...GatsbyImageSharpFixed
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
-// `
